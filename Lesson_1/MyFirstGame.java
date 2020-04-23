2a617da61de8d5bb2348f92fde208b54b6dc8df6
arthur1gust <gustaytsev@mail.ru>
2020-04-23 20:48:51 +0700

    MyFirstGame

--- Lesson_1/MyFirstGame.java
+++ Lesson_1/MyFirstGame.java
@@ -1,16 +1,17 @@
 public class MyFirstGame {
 	public static void main(String[] args) {
 		int m = 36;
-		if (m > 50) {
-			System.out.println("Vvedenoe vami chislo bolshe togo, chto zagadal computer");
-		} else if (m < 30) {
-			System.out.println("Vvedenoe vami chislo menshe togo, chto zagadal computer");
-		} else if (m >= 37 && m <= 50) {
-			System.out.println("Vvedenoe vami chislo bolshe togo, chto zagadal computer");
-		} else if (m >= 30 && m <= 35) {
-			System.out.println("Vvedenoe vami chislo menshe togo, chto zagadal computer");
-		} else {
-			System.out.println("Vy ugadali");
+
+		for (int i = 1; i <= 100; i++) {
+			int sum = i - m;
+			boolean result = sum < 0;
+			if (sum == 0) {
+				System.out.println("Вы угадали!");
+			} else if (result == true) {
+				System.out.println("Введенное вами число меньше того, что загадал компьютер");
+			} else {
+				System.out.println("Введенное вами число больше того, что загадал компьютер");
+			}
 		}
 	}
 }
