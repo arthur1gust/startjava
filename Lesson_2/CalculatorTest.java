46a7b2d5544a3aa9b7f31c09d0942ebe1677f9c5
A-ПК\A <gustaytsev@mail.ru>
2020-05-17 17:55:27 +0700

    Правка CalculatorTest

--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -4,25 +4,23 @@ public class CalculatorTest {
 	public static void main(String[] args) {
 		Calculator calc = new Calculator();
 		Scanner scan = new Scanner(System.in);
-		String yesAnswer = "yes";
 		String answer;
 
 		do {
-		System.out.print("Enter first number: ");
-		calc.setA(scan.nextInt());
+			System.out.print("Enter first number: ");
+			calc.setA(scan.nextInt());
 
-		System.out.print("Enter second number: ");
-		calc.setB(scan.nextInt());
+			System.out.print("Enter second number: ");
+			calc.setB(scan.nextInt());
 
-		System.out.print("Enter operation: ");
-		calc.setSign(scan.next().charAt(0));
+			System.out.print("Enter operation: ");
+			calc.setSign(scan.next().charAt(0));
 
-		System.out.println(calc.calculate());
+			System.out.println(calc.calculate());
 
-		System.out.print("Will you want continue? [yes/no]: ");
-		scan.nextLine();
-		answer = scan.nextLine();
-		}
-		while (answer.equalsIgnoreCase(yesAnswer));
+			System.out.print("Will you want continue? [yes/no]: ");
+			scan.nextLine();
+			answer = scan.nextLine();
+		} while (answer.equalsIgnoreCase("yes"));
 	}
 }
---
+++ Lesson_2/Jaeger.java
@@ -0,0 +1,27 @@
+public class Jaeger {
+
+	String modelName;
+	String mark;
+	String origin;
+	float height;
+	float weight;
+	int speed;
+	int strength;
+	int armor;
+
+	boolean drift() {
+
+	}
+
+	void move() {
+
+	}
+
+	String scanKaiju() {
+
+	}
+
+	void useVortexCannon() {
+
+	}
+}
