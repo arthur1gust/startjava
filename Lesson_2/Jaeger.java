2a1b2b2dfc063eb267ca1bd717c67f418db2102d
A-ПК\A <gustaytsev@mail.ru>
2020-05-18 15:46:42 +0700

    Jaeger

--- Lesson_2/Jaeger.java
+++ Lesson_2/Jaeger.java
@@ -1,24 +1,50 @@
 public class Jaeger {
+	private String robotName;
+	private String modelName;
+	private String mark;
+	private String origin;
+	private String status;
+	private float height;
+	private float weight;
+	private int speed;
+	private int strength;
+	private int armor;
 
-	String modelName;
-	String mark;
-	String origin;
-	float height;
-	float weight;
-	int speed;
-	int strength;
-	int armor;
+	public String getModelName() {
+		return modelName;
+	}
+
+	public void setModelName(String modelName) {
+		this.modelName = modelName;
+	}
+
+	public String getRobotName() {
+		return robotName;
+	}
+
+	public void setRobotName(String robotName) {
+		this.robotName = robotName;
+	}
+
+	public String getMark() {
+		return mark;
+	}
+
+	public void setMark(String mark) {
+		this.mark = mark;
+	}
 
 	boolean drift() {
-
+		System.out.println("You completed drift");
+		return true;
 	}
 
 	void move() {
-
+		System.out.println("You walked 5 steps");
 	}
 
 	String scanKaiju() {
-
+		return "nothing";
 	}
 
 	void useVortexCannon() {
---
+++ Lesson_2/JaegerTest.java
@@ -0,0 +1,19 @@
+public class JaegerTest {
+	public static void main(String[] args) {
+		Jaeger jaegerOne = new Jaeger();
+
+		jaegerOne.setModelName("Gipsy Avenger");
+		System.out.println(jaegerOne.getModelName());
+		jaegerOne.move();
+		jaegerOne.setMark("Mark-6");
+		System.out.println(jaegerOne.getMark());
+
+		Jaeger jaegerOne_2 = new Jaeger();
+
+		jaegerOne_2.setRobotName("Coyote Tango");
+		System.out.println(jaegerOne_2.getRobotName());
+		System.out.println(jaegerOne_2.drift());
+		jaegerOne_2.setMark("Mark-1");
+		System.out.println(jaegerOne_2.getMark());
+	}
+}
