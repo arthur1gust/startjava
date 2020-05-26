00f091c0cd7e37f8c7f4852c8b068dee7300e6b7
A-ПК\A <gustaytsev@mail.ru>
2020-05-26 12:21:55 +0700

    Jaeger

--- Lesson_2/Jaeger.java
+++ Lesson_2/Jaeger.java
@@ -5,10 +5,16 @@ public class Jaeger {
 	private String status;
 	private float height;
 	private float weight;
-	private int speed;
-	private int strength;
-	private int armor;
 
+	public Jaeger(String modelName, String mark, String origin, String status, float height, float weight) {
+		this.modelName = modelName;
+		this.mark = mark;
+		this.origin = origin;
+		this.status = status;
+		this.height = height;
+		this.weight = weight;
+	}
+	
 	public String getModelName() {
 		return modelName;
 	}
@@ -25,10 +31,36 @@ public class Jaeger {
 		this.mark = mark;
 	}
 
-	public Jaeger(String modelName, String mark, String origin){
-		this.modelName = modelName;
-		this.mark = mark;
+	public String getOrigin() {
+		return origin;
+	}
+
+	public void setOrigin(String origin) {
 		this.origin = origin;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public float getHeight() {
+		return height;
+	}
+
+	public void setHeight(float height) {
+		this.height = height;
+	}
+
+	public float getWeight() {
+		return weight;
+	}
+
+	public void setWeight(float weight) {
+		this.weight = weight;
 	}
 
 	boolean drift() {
--- Lesson_2/JaegerTest.java
+++ Lesson_2/JaegerTest.java
@@ -1,7 +1,7 @@
 public class JaegerTest {
 	public static void main(String[] args) {
-		Jaeger robotFirst = new Jaeger("Gipsy Avenger", "Mark-6", "United States of America");
-		Jaeger robotSecond = new Jaeger("Coyote Tango", "Mark-1", "United States of America");
+		Jaeger robotFirst = new Jaeger("Gipsy Avenger", "Mark-6", "United States of America", "Destroyed", 81.77F, 2.004F);
+		Jaeger robotSecond = new Jaeger("Coyote Tango", "Mark-1", "United States of America", "Destroyed", 85.34F, 2.312F);
 		
 		System.out.println(robotFirst);
 		System.out.println(robotSecond);
@@ -9,5 +9,6 @@ public class JaegerTest {
 		robotFirst.drift();
 		robotSecond.move();
 		System.out.println(robotSecond.scanKaiju());
+
 	}
 }
