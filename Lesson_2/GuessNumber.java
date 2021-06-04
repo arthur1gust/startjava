419c18c83963ebbf0849de508b8f3a4a816062be
Густайцев Артур Олегович <gustaytsev@mail.ru>
2021-06-04 11:53:54 +0700

    объявление переменных

--- Lesson_2/GuessNumber.java
+++ Lesson_2/GuessNumber.java
@@ -1,49 +1,27 @@
 import java.util.Scanner;
 
 public class GuessNumber {
-	String nameFirst;
-	String nameSecond;
-	int numberFirst;
-	int numberSecond;
+	private int numberComputer;
+	private Player playerFirst;
+    private Player playerSecond;
+	
 	Scanner scan = new Scanner(System.in);
 	GuessNumberTest gnt = new GuessNumberTest();
-
+	
+	public GuessNumber(Player playerFirst, Player playerSecond) {
+        this.playerFirst = playerFirst;
+        this.playerSecond = playerSecond;
+    }
+/*
 	private int random() {
-		int numberComputer;
 		numberComputer = (int) (Math.random() * 10);
 		return numberComputer;
 	}
-/*
-	private String changeNameFirst() {
-		nameFirst = scan.nextLine();
-		System.out.print("Enter first name");
-		return nameFirst;
-	}
-
-	private String changeNameSecond() {
-		nameSecond = scan.nextLine();
-		System.out.print("Enter second name");
-		return nameSecond;
-	}
-
-	private int changeNumberFirst() {
-		numberFirst = scan.nextInt();
-		System.out.print("Enter first number");
-		return numberFirst;
-	}
-
-	private int changeNumberSecond() {
-		numberSecond = scan.nextInt();
-		System.out.print("Enter second number");
-		return numberSecond;
-	}
-
-	Player playerFirst = new Player(changeNameFirst(), changeNumberFirst());
-	Player playerSecond = new Player(changeNameSecond(), changeNumberSecond());
 */
-	private String start() {
-		int sumFirst = random() - gnt.playerFirst.getNumber();
-		int sumSecond = random() - gnt.playerSecond.getNumber();
+	public void start() {
+		numberComputer = (int) (Math.random() * 100);
+		//int sumFirst = random() - gnt.playerFirst.getNumber();
+		//int sumSecond = random() - gnt.playerSecond.getNumber();
 		String resultFirst;
 		if (sumFirst == 0) {
 			resultFirst = "First player won!";
