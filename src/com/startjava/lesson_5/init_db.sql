a9459d74caed188e0fd32966f92e470f2299e454
Густайцев Артур Олегович <gustaytsev@mail.ru>
2021-07-21 16:53:21 +0700

    исправление отступов

--- src/com/startjava/lesson_5/init_db.sql
+++ src/com/startjava/lesson_5/init_db.sql
@@ -1,12 +1,12 @@
 INSERT INTO Jaegers (modelName, mark, height, weight, status, origin, launch, kaijuKill) 
 VALUES
-    ('Cherno Alpha',    'Mark-1', 85.34, 2.412, 'Destroyed', 'Russia',    '01.01.2015',   6),
-	('Gipsy Danger',    'Mark-3', 79.25, 1.980, 'Destroyed', 'USA',       '10.07.2017',     9),
-	('Striker Eureka',  'Mark-5', 76.2,  1.850, 'Destroyed', 'Australia', '02.11.2019', 11),
-	('Gipsy Avenger',   'Mark-6', 81.77, 2.004, 'Destroyed', 'USA',       '12.01.2034',  1),
-	('Crimson Typhoon', 'Mark-4', 76.2,  1.722, 'Destroyed', 'China',     '22.08.2018',   7),
-	('Coyote Tango',    'Mark-1', 85.34, 2.312, 'Destroyed', 'Japan',     '30.12.2015', 2),
-	('Apex',    'Mark-7', 100, 1.000, 'Active',    'Australia',       '20.12.2033', 0),
-    ('Atlas Destroyer',  'Mark-3', 200, 2000, 'Active',    'USA',       '01.12.2017', 1),
-    ('Saber Athena',  'Mark-7', 76.91, 1.628, 'Destroyed', 'USA',     '01.01.2033',  1),
-    ('Marauder Zeus',   'Mark-9', 300, 3.000, 'Destroyed', 'Germany',     '01.01.2100', 100);
+    ('Cherno Alpha', 'Mark-1', 85.34, 2.412, 'Destroyed', 'Russia', '01.01.2015', 6),
+    ('Gipsy Danger', 'Mark-3', 79.25, 1.980, 'Destroyed', 'USA', '10.07.2017', 9),
+    ('Striker Eureka', 'Mark-5', 76.2, 1.850, 'Destroyed', 'Australia', '02.11.2019', 11),
+    ('Gipsy Avenger', 'Mark-6', 81.77, 2.004, 'Destroyed', 'USA', '12.01.2034', 1),
+    ('Crimson Typhoon', 'Mark-4', 76.2, 1.722, 'Destroyed', 'China', '22.08.2018', 7),
+    ('Coyote Tango', 'Mark-1', 85.34, 2.312, 'Destroyed', 'Japan', '30.12.2015', 2),
+    ('Apex',    'Mark-7', 100, 1.000, 'Active', 'Australia', '20.12.2033', 0),
+    ('Atlas Destroyer', 'Mark-3', 200, 2000, 'Active', 'USA', '01.12.2017', 1),
+    ('Saber Athena', 'Mark-7', 76.91, 1.628, 'Destroyed', 'USA', '01.01.2033', 1),
+    ('Marauder Zeus', 'Mark-9', 300, 3.000, 'Destroyed', 'Germany', '01.01.2100', 100);
